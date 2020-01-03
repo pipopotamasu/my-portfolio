64969c97f2803bd80545ef043438cd05c58217a1
pipopotamasu <ec0156hx@gmail.com>
2020-01-03 14:48:51 +0900

    change seo info from default (#2)

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `pipopotamasu portfolio`,
+    description: `pipopotamasu's portfolio site`,
+    author: `@pipopotamasu`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -18,13 +18,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `pipopotamasu portfolio`,
+        short_name: `pipopotamasu portfolio`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#242424`,
+        theme_color: `#242424`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/pipopotamasu.jpg`,
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
